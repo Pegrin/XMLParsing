d4dd62a82a333d542516a064399c7918b25cf7ff
Pegrin <mail@wtiger.org>
2017-02-15 11:38:56 +0300

    Реализован ClassLoader и загрузка по ссылке.

---
+++ src/org/wtiger/inno/day5xmlparsing/classloader/JarClassLoader.java
@@ -0,0 +1,61 @@
+package org.wtiger.inno.day5xmlparsing.classloader;
+
+import sun.net.www.protocol.jar.URLJarFile;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.Hashtable;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+/**
+ * Created by olymp on 15.02.2017.
+ */
+public class JarClassLoader extends ClassLoader {
+    private String jarFile = "jar/test.jar"; //Path to the jar file
+    private Hashtable classes = new Hashtable(); //used to cache already defined classes
+
+    public JarClassLoader(String jarFile) {
+        super(JarClassLoader.class.getClassLoader()); //calls the parent class loader's constructor
+        this.jarFile = jarFile;
+    }
+
+    public Class loadClass(String className) throws ClassNotFoundException {
+        return findClass(className);
+    }
+
+    public Class findClass(String className) {
+        byte classByte[];
+        Class result = null;
+
+        result = (Class) classes.get(className); //checks in cached classes
+        if (result != null) {
+            return result;
+        }
+
+        try {
+            return findSystemClass(className);
+        } catch (Exception e) {
+        }
+
+        try {
+            JarFile jar = new JarFile(jarFile);
+            JarEntry entry = jar.getJarEntry(className + ".class");
+            InputStream is = jar.getInputStream(entry);
+            ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+            int nextValue = is.read();
+            while (-1 != nextValue) {
+                byteStream.write(nextValue);
+                nextValue = is.read();
+            }
+
+            classByte = byteStream.toByteArray();
+            result = defineClass(className, classByte, 0, classByte.length, null);
+            classes.put(className, result);
+            return result;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+}
--- src/org/wtiger/inno/day5xmlparsing/main/Main.java
+++ src/org/wtiger/inno/day5xmlparsing/main/Main.java
@@ -2,37 +2,73 @@ package org.wtiger.inno.day5xmlparsing.main;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.wtiger.inno.day5xmlparsing.anotherone.People;
+import org.wtiger.inno.day5xmlparsing.classloader.JarClassLoader;
+
 import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import java.io.File;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class Main {
+    private static final String pathToAnimal = "https://github.com/Pegrin/Animal/blob/master/Animal.jar?raw=true";
+    private static final String localJar = "tmp.jar";
+
+    public static Object getObjectFromURL(String path) {
+
+        return null;
+    }
 
     public static void main(String[] args) {
-        People p1 = new People("Петя", 18, 25000.00);
-        People p2 = new People("Виктор Иванович", 29, 45000.00);
-        SerializeToXML serializeToXML = new SerializeToXML();
-        Document doc = serializeToXML.getXMLDoc();
-        Element root = doc.createElement("root");
-        root.setAttribute("myMission", "serialized object by org.wtiger.inno.day5xmlparsing");
-        doc.appendChild(root);
-        serializeToXML.serialazToXML(p1, doc, root);
-        serializeToXML.serialazToXML(p2, doc, root);
-        File file = new File("test.xml");
-        Transformer transformer = null;
+
+        URL url = null;
+
         try {
-            transformer = TransformerFactory.newInstance().newTransformer();
-        } catch (TransformerConfigurationException e) {
-            e.printStackTrace();
-        }
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        try {
-            transformer.transform(new DOMSource(doc), new StreamResult(file));
-        } catch (TransformerException e) {
+            url = new URL(pathToAnimal);
+        } catch (MalformedURLException e) {
             e.printStackTrace();
         }
 
+        try (InputStream is = url.openStream()) {
+            Files.copy(is, Paths.get(localJar), StandardCopyOption.REPLACE_EXISTING);
+            JarClassLoader jarClassLoader = new JarClassLoader(localJar);
+            Class aClass = jarClassLoader.loadClass("Animal");
+            Object o = aClass.newInstance();
+//        People p1 = new People("Петя", 18, 25000.00);
+//        People p2 = new People("Виктор Иванович", 29, 45000.00);
+            SerializeToXML serializeToXML = new SerializeToXML();
+            Document doc = serializeToXML.getXMLDoc();
+            Element root = doc.createElement("root");
+            root.setAttribute("myMission", "serialized object by org.wtiger.inno.day5xmlparsing");
+            doc.appendChild(root);
+            serializeToXML.serialazToXML(o, doc, root);
+//        serializeToXML.serialazToXML(p2, doc, root);
+            File file = new File("test.xml");
+            Transformer transformer = null;
+            try {
+                transformer = TransformerFactory.newInstance().newTransformer();
+            } catch (TransformerConfigurationException e) {
+                e.printStackTrace();
+            }
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            try {
+                transformer.transform(new DOMSource(doc), new StreamResult(file));
+            } catch (TransformerException e) {
+                e.printStackTrace();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
     }
 }
